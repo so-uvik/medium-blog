8a3a2486263d3599fd58226a9490cca0926ec05a
so-uvik <souviksahamay27@gmail.com>
2024-08-29 17:26:09 +0530

    ye prisma generate maarke do

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Signup");
+});
+
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("signin");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("blog post route");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text("blog put route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("blog route with id as query parameter");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("blog bulk route");
+});
+
+export default app;
